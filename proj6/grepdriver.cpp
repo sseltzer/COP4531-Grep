5ec622a11360a9a84f7fd244161208c3599d1d27
Gus Maturana <gam11f@my.fsu.edu>
2014-12-03 22:16:12 -0500

    changes to grepdriver.cpp

--- proj6/grepdriver.cpp
+++ proj6/grepdriver.cpp
@@ -1,14 +1,30 @@
 
-
+#include "xstring.h"
 #include <iostream>
 #include "grep.h"
 
 //test
 int main(int argc, char* argv[])
 {
+<<<<<<< Updated upstream
     
     //format of grep command
     //GREP.x [flags] [pattern] [inFile] [outFile]
 
+=======
+	std::cout << "I did a thing! "<< argc << "\n";
+    fsu::String str;
+    
+    
+>>>>>>> Stashed changes
     return 1;
+}
+void GrepHelp()
+{
+    // Implementation according to flags
+    std::cout << "Regexp selection and interpretation:"
+              << "\n -E, --extended-regexp     PATTERN is an extended regular expression "
+              << "\n -F, --fixed-strings       PATTERN is a set of newline-separated strings ";
+
+    std::cout << std::endl;
 }
