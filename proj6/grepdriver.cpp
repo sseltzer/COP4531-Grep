bf7e831a771f791d718b049047543d6459badf2a
Matthew_Tannehill <mnt5623@my.fsu.edu>
2014-12-03 22:14:05 -0500

    Define GREP class
    added members and functions to grip class implementation

--- proj6/grepdriver.cpp
+++ proj6/grepdriver.cpp
@@ -1,9 +1,14 @@
 
 
 #include <iostream>
+#include "grep.h"
+
 //test
 int main(int argc, char* argv[])
 {
-	std::cout << "I did a thing! "<< argc << "\n";
+    
+    //format of grep command
+    //GREP.x [flags] [pattern] [inFile] [outFile]
+
     return 1;
 }
