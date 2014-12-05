b7cc137aa5b7aac7e8514522ba4f1a49f8796193
djkacevedo <djkacevedo@gmail.com>
2014-12-04 21:09:07 -0500

    Comments
    I cleaned things up a little and added comments.

--- proj6/grepdriver.cpp
+++ proj6/grepdriver.cpp
@@ -48,7 +48,8 @@ int main(int argc, char* argv[])
 	
 	//currently, it stores all flags in flags, it stores the pattern in PATTERN, it stores the data from all files listed in data, it can recognize a directory and iterate through it, but it can't get the files from it.
 	
-			//this is just for testing
+	//this is just for testing
+	
 	for (int i = 0; i < flags.size(); i ++)
 		cout<<"flag :" << flags[i]<<endl;
 		
@@ -61,44 +62,11 @@ int main(int argc, char* argv[])
 		for (int j = 0; j < data[i].size(); j++)
 			cout<<"line :"<<data[i][j]<<endl;
 	
-	
-
-    
-    //format of grep command
-    //GREP.x [flags] [pattern] [inFile] [outFile]
-  /*
-    fstring help = "-help";
-    // Error
-    if (argc == 3)
-    {
-        std::ifstream file(argv[2]);
-        if (file.fail())
-        {
-            std::cout << argv[2] << " File does not exist ";
-            std::cout << std::endl;
-            return 1;
-        }
-    }
-    if (argc == 1){
-        CommandFormat();
-        return 1;
-    }
-    if (help.compare(argv[1]) == 0)
-    {
-        std::cout<< "Help arguments";
-        GrepHelp();
-        return 1;
-    }
-    */
-
-	std::cout << "I did a thing! "<< argv[1] << " "<< argv [2] << "\n";
-   // fstring str;
-    
     
     return 1;
 }
 
-bool addDirectories (char* directory){			// this doesn't work yet
+bool addDirectories (char* directory){					// this doesn't work yet and is likely unneeded in keeping in the behaviour of grep on linprog
 	
 	char buf[PATH_MAX + 1]; 
 	struct stat st;
@@ -124,11 +92,11 @@ bool addDirectories (char* directory){			// this doesn't work yet
     return 1;
 }
 
-bool getData(int k) {
+bool getData(int k) {					// works recursively upward from fileNames[k] and puts their data in data[k]
 	
-	vector<string> file;
+	vector<string> file;				// temporary vector to hold the data from the current file
 	string line;
-	ifstream myfile (fileNames[k]);
+	ifstream myfile (fileNames[k]);		
 	
 	while (getline(myfile, line)){
 		file.push_back(line);
@@ -136,19 +104,19 @@ bool getData(int k) {
 	
 	data.push_back(file);
 	
-	if (k == fileNames.size() - 1) return true;
-	if (k < fileNames.size() - 1) return getData(k + 1);
+	if (k == fileNames.size() - 1) return true;				// stops the function at the last filename
+	if (k < fileNames.size() - 1) return getData(k + 1);	// calls the function for the next filename
 	
 	return false;
 }
 
 bool setInput (int argc, char* argv[]){
 	struct stat st;
-	bool lastFlag = false;
-	bool havePattern = false;
+	bool lastFlag = false;			// this is set to true if there are no options or once something that is not formatted as an option has been found
+	bool havePattern = false;		// this is set to true when PATTERN stores the pattern
 	
 	int k = 1;
-	if (argv[1][0] != '-') {		// this fixes the case of no flags
+	if (argv[1][0] != '-') {		// if there are no flags store the pattern and set the above bools
 		k = 2;
 		PATTERN = argv[1];
 		lastFlag = true;
@@ -156,13 +124,13 @@ bool setInput (int argc, char* argv[]){
 	}
 	
 	for (size_t i = k; i < argc; i++){
-		if (argv[i][0] == '-' && lastFlag == false){
-			if (argv[i][1] != '-')
+		if (argv[i][0] == '-' && lastFlag == false){			// if argv[i] is a flag and there hasn't been a non-flag then add it to flags
+			if (argv[i][1] != '-')								// this is for a single dash flag
 				flags.push_back(argv[i] + 1);
 			else
-				flags.push_back(argv[i] + 2);
+				flags.push_back(argv[i] + 2);					// this is for a -- flag
 				
-			if (i == (argc - 1)) {
+			if (i == (argc - 1)) {								// this is if a flag is the last entry instead of a pattern
                 CommandFormat();
 				return false;
 				}
@@ -170,25 +138,25 @@ bool setInput (int argc, char* argv[]){
 		}
 		else if (lastFlag == true && argv[i][0] == '-'){
             CommandFormat();
-			return false;						//either a flag without - or a flag after a file
+			return false;										// this is if a flag shows up after a non-flag
 		}
 		else if (lastFlag == true && havePattern == false){
 			PATTERN = argv[i];
 			havePattern = true;
 			if (i == argc - 1) {
-				cout << "reading from standard in is not implimented" << endl;
-				return false;   // have flags and pattern need to get data from std in
+				cout << "reading from standard in is not implemented" << endl;
+				return false;   								// have flags and pattern need to get data from std in
 			}
 		}
 		else if (lastFlag == true && havePattern == true){
 			stat(argv[i], &st);
-			if (st.st_mode & S_IFDIR){
-				addDirectories(argv[i]);		// get files in directory
+			if (st.st_mode & S_IFDIR){							// if the filename is for a directory search through the directory and add all files in it to fileNames
+				//addDirectories(argv[i]);						// this probably won't be implemented as it isn't implemented on grep when used on linprog
 			}
-			else if (st.st_mode & S_IFREG){
+			else if (st.st_mode & S_IFREG){						// if the filename belongs to a file added it to fileNames
 				fileNames.push_back(argv[i]);
 			}
-			else {
+			else {												// if the filename is invalid give an error
 				cout << "invalid file" << endl;
 				return false;
 			}
