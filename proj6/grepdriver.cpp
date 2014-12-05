c3df853866b715af9268011f41073a521c31f446
Matthew_Tannehill <mnt5623@my.fsu.edu>
2014-12-05 14:11:30 -0500

    sync

--- proj6/grepdriver.cpp
+++ proj6/grepdriver.cpp
@@ -2,33 +2,11 @@
 #include <iostream>
 #include "grep.h"
 
-typedef std::string            String;
-typedef fsu::Vector<String>    Vector;
-//test
 int main(int argc, char* argv[])
 {
-    Vector filenames;
-    //format of grep command
-    //GREP.x [flags] [pattern] [inFile] [outFile]
 
-    Grep gp;
-    std::string string;
-    char * array;
     
-    filenames.PushBack("movies_test.txt");
-    //filenames.PushBack("file 2.txt");
-    //filenames.PushBack("file 3.txt");
-    
-    if(!gp.search(array))
-        std::cout << "Search failed\n";
-    else
     {
-        gp.processing(filenames);
-        if (gp.search(NULL))
-        {
-        std::cout << "Search Invalid\n";
-        }
-    }
     
     return 1;
 }
