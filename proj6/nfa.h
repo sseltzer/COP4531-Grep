5757e07b570bbbdd2f99ebdddb53e577655fab5b
Sean <seltzerk@tampabay.rr.com>
2014-12-06 22:27:09 -0500

    Updated NFA (now with more working code!).

--- proj6/nfa.h
+++ proj6/nfa.h
@@ -17,15 +17,18 @@ class NFA
 		bool Matches				(const char* line);
 
 	private:
+		void AdvanceToNonControl(Vertex state, fsu::Vector<Vertex> &newStates);
+		bool IsValidCharacter(const char* cur, char pattern);
 		Vector v_;
 		Graph g_;
 };
 NFA::NFA(const char* pattern) {
-	std::cout << pattern << "\n";
 	NFA::PatternToNFA(pattern);
 }
 void NFA::PatternToNFA(const char* pattern) {
+	v_.PushBack('(');
   	for(const char* c = pattern; *c; ++c) v_.PushBack(*c);
+  	v_.PushBack(')');
   	g_.SetVrtxSize(v_.Size());
 
   	Stack stack;
@@ -54,7 +57,6 @@ void NFA::PatternToNFA(const char* pattern) {
   			case '*':
   				g_.AddEdge(i, i + 1);
   				if (i >= v_.Size()) continue;
-  				//g_.AddEdge(index - 1, i);
   				g_.AddEdge(i, index - 1);
   				break;
   			default:
@@ -62,86 +64,49 @@ void NFA::PatternToNFA(const char* pattern) {
   				break;
   		}
   	}
-
-    v_.Dump(std::cout);
-    g_.Dump(std::cout);
-	std::cout << "\n";
 }
 
 bool NFA::Matches(const char* line) {
-
-	Vertex pos = 0;
-	
-	fsu::Vector<Graph::AdjIterator> stack;
-	fsu::Vector<Vertex> history;
-	//Stack stack;
-	Stack alt;
-
-	typename Graph::AdjIterator edge;
-	
-	for (; pos < v_.Size(); ++pos) {
-		if (g_.EdgeSize() > 1) stack.PushBack(g_.Begin(pos));
-	}
-
-	const char* c = line;
-	pos = 0;
-	history.PushBack(pos);
-	for(; *c; ++c) {
-		if (pos == v_.Size()) return true;
-		std::cout << "*c: " << *c << " v_[pos]: " << v_[pos] << " pos: " << pos;
-		std::cout << "\nhistory: \n";
-		history.Dump(std::cout);
-		if (v_[pos] == '(' || v_[pos] == '|' || v_[pos] == ')' || v_[pos] == '*') {
-			std::cout << " skipping\n";
-			pos = *stack[pos];
-			history.PushBack(pos);
-			--c;
-			std::cout << " end skipping\n";
-			continue;
-		}
-		std::cout << "\n";
-		if (*c == v_[pos]) {
-			if (*stack[pos] == stack.Size()) return true;
-			if (*stack[pos] < v_.Size() && v_[*stack[pos]] == '*') {
-				std::cout << "found closure\n";
-				while (*c == v_[pos]) ++c;
-				--c;
-				pos = *stack[pos];
-				history.PushBack(pos);
+	fsu::Vector<Vertex> states;
+	fsu::Vector<Vertex> newStates;
+	NFA::AdvanceToNonControl(0, states);
+	for(const char* c = line; *c; ++c) {
+		for (size_t s = 0; s < states.Size(); ++s) {
+			if (states[s] == v_.Size()) return true;
+			if (NFA::IsValidCharacter(c, v_[states[s]])) {
+				NFA::AdvanceToNonControl(states[s], newStates);
 				continue;
 			}
-			if (pos == stack.Size()) return true;
-			pos = *stack[pos];
-			history.PushBack(pos);
-
-		} else {
-			if (*stack[pos] < v_.Size() && v_[*stack[pos]] == '*') continue;
-			pos = 0;
 		}
-
-
-		if (++c == '\0') {
-			c = line;
-			std::cout << "Popping History: \n";
-			std::cout << pos << "\n";
-			while(history.Size() > 0) {
-				pos = history.Back();
-				history.PopBack();
-				if (v_[pos] == '*') {
-					pos = history.Back();
-					history.PopBack();
-					continue;
-				}
-				++(stack[pos]);
-				std::cout << pos << "\n";
-				if (stack[pos] != g_.End(pos)) break;
-			}
-			if (history.Size() == 0) return false;
-		}
-		--c;
+		states.Clear();
+		for (size_t s = 0; s < newStates.Size(); ++s) states.PushBack(newStates[s]);
+		newStates.Clear();
+		NFA::AdvanceToNonControl(0, states);
+		if (*(c + 1) == '\0') c--;
 	}
-
-	//stack.Dump(std::cout);
-	//*/
 	return false;
+}
+
+bool NFA::IsValidCharacter(const char* cur, char pattern) {
+	if (*cur == pattern) return true;
+	if (pattern == '.') return true;
+	if (pattern == '(') return true;
+	if (pattern == ')') return true;
+	if (pattern == '|') return true;
+	if (pattern == '*') return true;
+	return false;
+}
+
+void NFA::AdvanceToNonControl(Vertex state, fsu::Vector<Vertex> &newStates) {
+	if (state == v_.Size()) return;
+	Graph::AdjIterator i = g_.Begin(state);
+	for (; i != g_.End(state); ++i) {
+		if (*i == v_.Size()) {
+			newStates.PushBack(*i);
+			return;
+		}
+		char thisChar = v_[*i];
+		if (thisChar == '(' || thisChar == '|' || thisChar == ')' || thisChar == '*') NFA::AdvanceToNonControl(*i, newStates);
+		else newStates.PushBack(*i);
+	}
 }
