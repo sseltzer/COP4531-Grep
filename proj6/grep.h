551022ec5cb2e3c587db2701c7503309ded50e04
Sean <seltzerk@tampabay.rr.com>
2014-12-07 00:52:58 -0500

    Formatting changes.

--- proj6/grep.h
+++ proj6/grep.h
@@ -1,150 +1,151 @@
+/*
+    grep.h
+    12/06/14
+    Matthew Tannehill
 
 
+    -n, --line-number
+    Each output line is preceded by its relative line number in the file, starting at line 1.  The line
+    number counter is reset for each file processed.  This option is ignored if -c, -L, -l, or -q is
+    specified.
 
+    -l, --files-with-matches
+    Only the names of files containing selected lines are written to standard output.  grep will only
+    search a file until a match has been found, making searches potentially less expensive.  Pathnames
+    are listed once per file searched.  If the standard input is searched, the string ``(standard
+    input)'' is written.
 
+    -i, --ignore-case
+    Perform case insensitive matching.  By default, grep is case sensitive.
+
+    -V, --version
+    Display version information and exit.
+
+*/
 #ifndef _GREP_H
 #define _GREP_H
 
-#include "vector.h"
+#include <vector.h>
 #include <string>
 #include <fstream>
-#include "nfa.h"
+#include <nfa.h>
 
 #define NUM_OF_FLAGS 10;
 
 
 namespace grep
 {
-class Grep
-{
-public:
-    typedef std::string            String;
-    typedef fsu::Vector<String>    Vector;
-
-           Grep       ();
-    bool   setFlag    (const char* flag);
-    bool   processing (const Vector files);
-    Vector results    ();
-    bool   search     (char* search);
-private:
-    bool   flagSet_[10];
-    Vector results_;
-    Vector flag_;
-    const char * search_ = NULL;
-    
-};
-
-Grep::Grep()
-{
-    for( size_t ctr = 0; ctr < 10; ++ctr )
-        flagSet_[ctr] = false;
-}
-
-bool Grep::setFlag(const char* flagSet)
-{
-    flagSet_[0] = true;//-i
-    flagSet_[1] = true;//-l
-    flagSet_[2] = true;//-n
-    //std::cout << "Set " << flagSet << " flag\n";
-        return true;
-}
-
-bool Grep::processing (Vector files)
-{
-    
-    std::ifstream inStream;
-    String line,temp;
-    size_t lineNumber = 0;
-   
-    
-    for (size_t counter = 0; counter < files.Size(); ++counter)
+    class Grep
     {
-        //open file
-        inStream.open(files[counter]);
-        if (inStream.fail())
-        {
-            std::cout << "grep: " << files[counter] << ": No such file or directory\n";
-            return false;
-        }
+    public:
+        typedef std::string            String;
+        typedef fsu::Vector<String>    Vector;
+
+               Grep       ();
+        bool   setFlag    (const char* flag);
+        bool   processing (const Vector files);
+        Vector results    ();
+        bool   search     (char* search);
+    private:
+        bool   flagSet_[10];
+        Vector results_;
+        Vector flag_;
+        const char * search_ = NULL;
         
-        if (search_ == NULL)
-        {
-            std::cout << "Search parameter not set\n";
-            return false;
-        }
+    };
+
+    Grep::Grep()
+    {
+        for( size_t ctr = 0; ctr < 10; ++ctr )
+            flagSet_[ctr] = false;
+    }
+
+    bool Grep::setFlag(const char* flagSet)
+    {
+        flagSet_[0] = true;//-i
+        flagSet_[1] = true;//-l
+        flagSet_[2] = true;//-n
+        //std::cout << "Set " << flagSet << " flag\n";
+            return true;
+    }
+
+    bool Grep::processing (Vector files)
+    {
         
-        NFA nfa(search_);
-        if (flagSet_[2] == true)
-        {   //add line with file name
-            results_.PushBack(files[counter]);
-        }
-        while (!inStream.eof())
+        std::ifstream inStream;
+        String line,temp;
+        size_t lineNumber = 0;
+       
+        
+        for (size_t counter = 0; counter < files.Size(); ++counter)
         {
-            std::getline(inStream,line,'\n');
-            ++lineNumber;
-            
-            if( nfa.Matches( line.c_str() ) )
+            //open file
+            inStream.open(files[counter]);
+            if (inStream.fail())
             {
-                if(flagSet_[1] == true)
-                {   //add line number
-                    temp = "Line [" + std::to_string(lineNumber) + "] : " + line;
-                    line = temp ;
-                }
-                if (flagSet_[2] == true)
-                {
-                    line = "    " + line;
-                }
-                //if flag set add line number to front of line
-                results_.PushBack(line);
+                std::cout << "grep: " << files[counter] << ": No such file or directory\n";
+                return false;
             }
-            line.clear();
+            
+            if (search_ == NULL)
+            {
+                std::cout << "Search parameter not set\n";
+                return false;
+            }
+            
+            Grep::NFA nfa(search_);
+            if (flagSet_[2] == true)
+            {   //add line with file name
+                results_.PushBack(files[counter]);
+            }
+            while (!inStream.eof())
+            {
+                std::getline(inStream,line,'\n');
+                ++lineNumber;
+                
+                if( nfa.Matches( line.c_str() ) )
+                {
+                    if(flagSet_[1] == true)
+                    {   //add line number
+                        temp = "Line [" + std::to_string(lineNumber) + "] : " + line;
+                        line = temp ;
+                    }
+                    if (flagSet_[2] == true)
+                    {
+                        line = "    " + line;
+                    }
+                    //if flag set add line number to front of line
+                    results_.PushBack(line);
+                }
+                line.clear();
+            }
+            lineNumber = 0;
         }
-        lineNumber = 0;
+        return true;
     }
-    return true;
-}
-    
-Grep::Vector Grep::results()
-{
-    return results_;
-}
-
-bool Grep::search(char* search)
-{
-    
-    size_t ind = 0;
-    if (sizeof(*search) == 0) { std::cout << "Search is zero length\n "; return false;}
-    if (search == NULL)       { std::cout << "Search pointer is NULL\n"; return false;}
-    if (*search == '\0')      { std::cout << "'\\0' string\n          "; return false;}
-    
-    if (flagSet_[0] == true)
+        
+    Grep::Vector Grep::results()
     {
-        while (strlen(search) > ind)
-        {
-            search[ind] = std::tolower(search[ind]);
-            ++ind;
-        }
+        return results_;
     }
-    search_ = search;
-    return true;
+
+    bool Grep::search(char* search)
+    {
+        size_t ind = 0;
+        if (sizeof(*search) == 0) { std::cout << "Search is zero length\n "; return false;}
+        if (search == NULL)       { std::cout << "Search pointer is NULL\n"; return false;}
+        if (*search == '\0')      { std::cout << "'\\0' string\n          "; return false;}
+        
+        if (flagSet_[0] == true)
+        {
+            while (strlen(search) > ind)
+            {
+                search[ind] = std::tolower(search[ind]);
+                ++ind;
+            }
+        }
+        search_ = search;
+        return true;
+    }
 }
-}
-#endif
-/*
- -n, --line-number
- Each output line is preceded by its relative line number in the file, starting at line 1.  The line
- number counter is reset for each file processed.  This option is ignored if -c, -L, -l, or -q is
- specified.
- 
- -l, --files-with-matches
- Only the names of files containing selected lines are written to standard output.  grep will only
- search a file until a match has been found, making searches potentially less expensive.  Pathnames
- are listed once per file searched.  If the standard input is searched, the string ``(standard
- input)'' is written.
- 
- -i, --ignore-case
- Perform case insensitive matching.  By default, grep is case sensitive.
- 
- -V, --version
- Display version information and exit.*/
- 
+#endif 
--- proj6/nfa.h
+++ proj6/nfa.h
@@ -1,113 +1,129 @@
+/*
+    nfa.h
+    12/06/14
+    Sean Seltzer
+
+*/
+#ifndef _NFA_H
+#define _NFA_H
+
+
 #include <iostream>
-#include "vector.h"
-#include "graph.h"
-#include "xstring.h"
+#include <vector.h>
+#include <graph.h>
+#include <xstring.h>
 
+namespace Grep {
+	class NFA
+	{
+		public:
+			typedef uint32_t Vertex;
+			typedef fsu::Vector<char> Vector;
+			typedef fsu::Vector<size_t> Stack;
+			typedef fsu::Vector<Vertex> State;
+			typedef fsu::ALDGraph<Vertex> Graph;
 
+			NFA 				(const char* pattern);
+			void PatternToNFA	(const char* pattern);
+			bool Matches		(const char* line);
 
-class NFA
-{
-	public:
-    typedef uint32_t Vertex;
-    typedef fsu::Vector<char> Vector;
-    typedef fsu::Vector<size_t> Stack;
-    typedef fsu::Vector<Vertex> State;
-    typedef fsu::ALDGraph<Vertex> Graph;
-		NFA 						(const char* pattern);
-		void PatternToNFA			(const char* pattern);
-		bool Matches				(const char* line);
+		private:
+			void AdvanceToNonControl(Vertex state, State &newStates);
+			bool IsValidCharacter(const char* cur, char pattern);
+			Vector v_;
+			Graph g_;
+	};
+	NFA::NFA(const char* pattern) {
+		NFA::PatternToNFA(pattern);
+	}
+	void NFA::PatternToNFA(const char* pattern) {
+		v_.PushBack('(');
+	  	for(const char* c = pattern; *c; ++c) v_.PushBack(*c);
+	  	v_.PushBack(')');
+	  	g_.SetVrtxSize(v_.Size());
 
-	private:
-		void AdvanceToNonControl(Vertex state, State &newStates);
-		bool IsValidCharacter(const char* cur, char pattern);
-		Vector v_;
-		Graph g_;
-};
-NFA::NFA(const char* pattern) {
-	NFA::PatternToNFA(pattern);
-}
-void NFA::PatternToNFA(const char* pattern) {
-	v_.PushBack('(');
-  	for(const char* c = pattern; *c; ++c) v_.PushBack(*c);
-  	v_.PushBack(')');
-  	g_.SetVrtxSize(v_.Size());
-
-  	Stack stack;
-  	size_t i, index, back;
-  	for(i = 0; i < v_.Size(); ++i) {
-  		index = i;
-  		switch(v_[i]) {
-  			case '(':
-  				g_.AddEdge(i, i + 1);
-  				stack.PushBack(i);
-  				break;
-  			case ')':
-				back = stack.Back();
-				stack.PopBack();
-				if (v_[back] == '|') {
-					index = stack.Back();
+	  	Stack stack;
+	  	size_t i, index, back;
+	  	for(i = 0; i < v_.Size(); ++i) {
+	  		index = i;
+	  		switch(v_[i]) {
+	  			case '(':
+	  				g_.AddEdge(i, i + 1);
+	  				stack.PushBack(i);
+	  				break;
+	  			case ')':
+					back = stack.Back();
 					stack.PopBack();
-					g_.AddEdge(back, i);
-					g_.AddEdge(index, back + 1);
-				}
-				g_.AddEdge(i, i + 1);
-  				break;
-  			case '|':
-  				stack.PushBack(i);
-  				break;
-  			case '*':
-  				g_.AddEdge(i, i + 1);
-  				if (i >= v_.Size()) continue;
-  				g_.AddEdge(i, index - 1);
-  				break;
-  			default:
-  				g_.AddEdge(i, i + 1);
-  				break;
-  		}
-  	}
-}
+					if (v_[back] == '|') {
+						index = stack.Back();
+						stack.PopBack();
+						g_.AddEdge(back, i);
+						g_.AddEdge(index, back + 1);
+					}
+					g_.AddEdge(i, i + 1);
+	  				break;
+	  			case '|':
+	  				stack.PushBack(i);
+	  				break;
+	  			case '*':
+	  				g_.AddEdge(i, i + 1);
+	  				if (i >= v_.Size()) continue;
+	  				g_.AddEdge(i, index - 1);
+	  				break;
+	  			default:
+	  				g_.AddEdge(i, i + 1);
+	  				break;
+	  		}
+	  	}
+	}
 
-bool NFA::Matches(const char* line) {
-	State states;
-	State newStates;
-	NFA::AdvanceToNonControl(0, states);
-	for(const char* c = line; *c; ++c) {
-		for (size_t s = 0; s < states.Size(); ++s) {
-			if (states[s] == v_.Size()) return true;
-			if (NFA::IsValidCharacter(c, v_[states[s]])) {
-				NFA::AdvanceToNonControl(states[s], newStates);
-				continue;
-			}
-		}
-		states.Clear();
-		for (size_t s = 0; s < newStates.Size(); ++s) states.PushBack(newStates[s]);
-		newStates.Clear();
+	bool NFA::Matches(const char* line) {
+		State states;
+		State newStates;
 		NFA::AdvanceToNonControl(0, states);
-		if (*(c + 1) == '\0') c--;
-	}
-	return false;
-}
-
-bool NFA::IsValidCharacter(const char* cur, char pattern) {
-	if (*cur == pattern) return true;
-	if (pattern == '.') return true;
-	if (pattern == '(') return true;
-	if (pattern == ')') return true;
-	if (pattern == '|') return true;
-	if (pattern == '*') return true;
-	return false;
-}
-
-void NFA::AdvanceToNonControl(Vertex state, State &newStates) {
-	if (state == v_.Size()) return;
-	Graph::AdjIterator i = g_.Begin(state);
-	for (; i != g_.End(state); ++i) {
-		if (*i == v_.Size()) {
-			newStates.PushBack(*i);
-			return;
+		for(const char* c = line; *c; ++c) {
+			for (size_t s = 0; s < states.Size(); ++s) {
+				if (states[s] == v_.Size()) return true;
+				if (NFA::IsValidCharacter(c, v_[states[s]])) {
+					NFA::AdvanceToNonControl(states[s], newStates);
+					continue;
+				}
+			}
+			states.Clear();
+			for (size_t s = 0; s < newStates.Size(); ++s) states.PushBack(newStates[s]);
+			newStates.Clear();
+			NFA::AdvanceToNonControl(0, states);
+			if (*(c + 1) == '\0') c--;
 		}
-		char thisChar = v_[*i];
-		if (thisChar == '(' || thisChar == '|' || thisChar == ')' || thisChar == '*') NFA::AdvanceToNonControl(*i, newStates);
-		else newStates.PushBack(*i);
+		return false;
+	}
+
+	bool NFA::IsValidCharacter(const char* cur, char pattern) {
+		if (*cur == pattern) return true;
+		if (pattern == '.') return true;
+		if (pattern == '(') return true;
+		if (pattern == ')') return true;
+		if (pattern == '|') return true;
+		if (pattern == '*') return true;
+		return false;
+	}
+
+	void NFA::AdvanceToNonControl(Vertex state, State &newStates) {
+		if (state == v_.Size()) return;
+		Graph::AdjIterator i = g_.Begin(state);
+		for (; i != g_.End(state); ++i) {
+			if (*i == v_.Size()) {
+				newStates.PushBack(*i);
+				return;
+			}
+			char thisChar = v_[*i];
+			if (thisChar == '(' || 
+				thisChar == '|' || 
+				thisChar == ')' || 
+				thisChar == '*') 
+					NFA::AdvanceToNonControl(*i, newStates);
+			else newStates.PushBack(*i);
+		}
 	}
 }
+#endif
